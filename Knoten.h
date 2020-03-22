f8b447a1068ddbdd67b88659d13356a0c288c6a4
MCDA18 <62431770+MCDA18@users.noreply.github.com>
2020-03-22 22:30:16 +0200

    asta ii proiectu

--- Knoten.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Knoten
-{
-public:
-	int val;//valoarea pe care o va avea un nod
-	Knoten* right_son;//fiu dreapta
-	Knoten* left_son;//fiu stanga
-};
---
+++ Laborator3_oop/Laborator_3_oop/Knoten.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Knoten
+{
+public:
+	int val;//valoarea pe care o va avea un nod
+	Knoten* right_son;//fiu dreapta
+	Knoten* left_son;//fiu stanga
+};
